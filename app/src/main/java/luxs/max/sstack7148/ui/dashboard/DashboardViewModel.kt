3fc2df397e73cd9f5493a044d610d2d412447f58
Maksim <max.1172.luk@gmail.com>
2021-02-08 20:38:13 +0300

    git init

---
+++ app/src/main/java/luxs/max/sstack7148/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package luxs.max.sstack7148.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
